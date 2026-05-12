8aee754d762248e52ef717c15ae0417c2f505eec
santtyy <gasttonalmeyyda@gmail.com>
2026-05-12 20:36:10 -0300

    excluindo jogo

--- jogo/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { Assasino } from "./Assasino.ts";
-import { Jogo } from "./Jogo.ts";
-import { Mago } from "./Mago.ts";
-
-let mago: Mago = new Mago("Mago", 100, 20, 30);
-let assasino: Assasino = new Assasino("Assasino",100,10,15,15);
-
-let jogo:Jogo = new Jogo();
-jogo.inicia(mago,assasino);
